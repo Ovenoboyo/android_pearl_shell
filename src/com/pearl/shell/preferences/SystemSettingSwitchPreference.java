65ad1fae4ab674f70e2bbdb82190b6c504fc9850
Dixzz <vishusatyam36@gmail.com>
2019-01-22 17:11:05 +0000

    Misc changes
    Signed-off-by: Dixzz <vishusatyam36@gmail.com>

--- src/com/pearl/shell/preferences/SystemSettingSwitchPreference.java
+++ src/com/pearl/shell/preferences/SystemSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.serein.extras.preferences;
+package com.pearl.shell.preferences;
 
 import android.content.Context;
 import android.support.v14.preference.SwitchPreference;
